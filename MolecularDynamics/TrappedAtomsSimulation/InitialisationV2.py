2a9d07bca255cfb717c41a480276c86d348dfd57
christpaul94 <christpaul94@gmail.com>
2026-03-14 16:21:48 +0100

    Update pair parameters in InitialisationV2.py

--- MolecularDynamics/TrappedAtomsSimulation/InitialisationV2.py
+++ MolecularDynamics/TrappedAtomsSimulation/InitialisationV2.py
@@ -92,7 +92,7 @@ def initialize_one_temp_gaussian_state(
     omega_matrix_dimless = torch.diag(omega_dimless)
 
     # --- Wechselwirkungsparameter ---
-    pair_params = {'r': 0.0, 'c': 0.0}
+    pair_params = {'sigma': 0.0, 'V0': 0.0}
     if r0_phys is not None and C_phys is not None:
         pair_params['sigma'] = float(r0_phys / L0)
         pair_params['V0'] = float(C_phys / E0)
